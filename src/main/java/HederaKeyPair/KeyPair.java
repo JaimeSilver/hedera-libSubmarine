b11399c99c9fffc1c9d49351570c5df189ef5575
Jaime Plata <jp@aochain.net>
2019-08-01 22:01:40 -0400

    Prepare Upgrade
    Clean repository

--- src/main/java/HederaKeyPair/KeyPair.java
+++
@@ -1,9 +0,0 @@
-package HederaKeyPair;
-
-public interface KeyPair {
-    byte[] getPrivateKey();
-    byte[] getPublicKey();
-    byte[] getPublicKeyEncoded();
-    byte[] signMessage(byte[] message);
-    boolean verifySignature(byte[] message, byte[] signature);
-}
